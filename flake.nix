6b2ce9d4d28c479a889e3b0bca036e95f09092a3
Locochoco <ivanrwpf@gmail.com>
2023-12-16 19:28:39 -0300

    fixing overlay and flake to make the overlay work

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
   description = "A Nix flake for some Windows apps";
-
   inputs = {
     erosanix.url = "github:emmanuelrosa/erosanix";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     nixpkgs.url = "github:NixOS/nixpkgs/master";
   };
-
   outputs = { self, flake-utils, flake-compat, erosanix, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+      erosanix-lib-overlay = final: prev: {
+        mkWindowsApp = erosanix.lib.${system}.mkWindowsApp;
+        makeDesktopIcon = erosanix.lib.${system}.makeDesktopIcon;
+        copyDesktopIcons = erosanix.lib.${system}.copyDesktopIcons;
+      };
         pkgs = (import nixpkgs) {
           inherit system;
-	  overlays = [ self.overlay.wineApps ];
+	  overlays = [ self.overlay.wineApps erosanix-lib-overlay];
 	};
       in rec {
         packages = rec {
